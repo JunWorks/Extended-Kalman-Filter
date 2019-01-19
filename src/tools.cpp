50f767e535e6e5895b5a9242a0589be4dbf9c1e2
Jun Ma <majun0118@gmail.com>
2019-01-19 11:38:20 -0800

    CalculateJacobian

--- src/tools.cpp
+++ src/tools.cpp
@@ -21,4 +21,26 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+   MatrixXd Hj(3,4);
+   // recover state parameters
+   float px = x_state(0);
+   float py = x_state(1);
+   float vx = x_state(2);
+   float vy = x_state(3);
+
+   // TODO: YOUR CODE HERE
+   float rho = sqrt(px*px + py*py);
+   float rho2 = rho*rho;
+   float rho3 = rho2*rho;
+
+   // check division by zero
+   if (px == 0 || py ==0) {
+       cout << "Check px or py, they should be non-zero" << endl;
+   }else{
+       Hj << px/rho, py/rho, 0,0,
+          -py/rho2, px/rho2, 0,0,
+          py*(vx*py - vy*px)/rho3, px*(vy*px - vx*py)/rho3, px/rho, py/rho;
+      }
+   // compute the Jacobian matrix
+   return Hj;
 }
